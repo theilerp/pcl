63a94efc872951b35c297f7d97e212427f7ac78e
theilerp <theilerp@ethz.ch>
2015-05-06 11:59:14 +0200

    removed bug in kfpcs test caused by early termination after 1 try

--- test/registration/test_kfpcs_ia_data.h
+++ test/registration/test_kfpcs_ia_data.h
@@ -4,7 +4,7 @@
 const int nr_threads = 1;
 const float voxel_size = 0.1f;
 const float approx_overlap = 0.9f;
-const float abort_score = 1.0f;
+const float abort_score = 0.0f;
 
 const float transformation_office1_office2 [4][4] = {
   { -0.6946f, -0.7194f, -0.0051f, -3.6352f },
